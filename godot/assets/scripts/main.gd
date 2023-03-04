ffbfdf847ad92015233129923eb629c52bdb6370
Alex MacLean <maclean199@gmail.com>
2023-03-04 15:01:11 -0500

    Button makes campfire bigger

--- godot/assets/scripts/main.gd
+++ godot/assets/scripts/main.gd
@@ -14,7 +14,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_XRToolsInteractableAreaButton_button_pressed(button):
-	print("BUTTON PRESSED") # Replace with function body.
--- godot/assets/scripts/osc_sender.gd
+++ godot/assets/scripts/osc_sender.gd
@@ -2,16 +2,19 @@ extends Node
 
 var oscsndr
 var cube
+var cube2
 
 func _ready():
 	
-	cube = get_node( "../cube" )
+	cube = get_node("../cube")
+	cube2 = get_node("../cube2")
 	oscsndr = load("res://addons/gdosc/gdoscsender.gdns").new()
-	oscsndr.setup( "127.0.0.1", 12000 )		# will send messages to ip:port
+	oscsndr.setup( "192.168.2.147", 12000 )		# will send messages to ip:port
 	oscsndr.start()							# enabling emission
 
 func _process(delta):
 	cube.rotate_y( delta * 1 )
+	cube2.rotate_x( delta * 1 )
 	oscsndr.msg("/gdosc")					# creation of new message internally
 	oscsndr.add( cube )						# adding an object, will send its name
 	oscsndr.add( cube.rotation )			# adding a vector3, will be transform into 3 floats
