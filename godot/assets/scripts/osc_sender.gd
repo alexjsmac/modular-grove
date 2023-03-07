77fd0e1b6993c71e729ea889ee52b138df598abb
Alex MacLean <maclean199@gmail.com>
2023-03-07 10:40:27 -0500

    Cubes spin, adds slider

--- godot/assets/scripts/osc_sender.gd
+++ godot/assets/scripts/osc_sender.gd
@@ -5,22 +5,22 @@ var cube
 var cube2
 
 func _ready():
-	
+
 	cube = get_node("../cube")
 	cube2 = get_node("../cube2")
-	oscsndr = load("res://addons/gdosc/gdoscsender.gdns").new()
-	oscsndr.setup( "192.168.2.147", 12000 )		# will send messages to ip:port
-	oscsndr.start()							# enabling emission
+#	oscsndr = load("res://addons/gdosc/gdoscsender.gdns").new()
+#	oscsndr.setup( "192.168.2.147", 12000 )		# will send messages to ip:port
+#	oscsndr.start()							# enabling emission
 
 func _process(delta):
 	cube.rotate_y( delta * 1 )
 	cube2.rotate_x( delta * 1 )
-	oscsndr.msg("/gdosc")					# creation of new message internally
-	oscsndr.add( cube )						# adding an object, will send its name
-	oscsndr.add( cube.rotation )			# adding a vector3, will be transform into 3 floats
-	oscsndr.add( cube.translation )
-	oscsndr.send()							# sending the message
-	pass
-	
-func _exit_tree ( ):
-	oscsndr.stop()							# disable the sender, highly recommended!
+#	oscsndr.msg("/gdosc")					# creation of new message internally
+#	oscsndr.add( cube )						# adding an object, will send its name
+#	oscsndr.add( cube.rotation )			# adding a vector3, will be transform into 3 floats
+#	oscsndr.add( cube.translation )
+#	oscsndr.send()							# sending the message
+#	pass
+#
+#func _exit_tree ( ):
+#	oscsndr.stop()							# disable the sender, highly recommended!
--- godot/assets/scripts/push_button.gd
+++ godot/assets/scripts/push_button.gd
@@ -5,7 +5,7 @@ var oscsndr
 func _ready():
 	
 	oscsndr = load("res://addons/gdosc/gdoscsender.gdns").new()
-	oscsndr.setup( "127.0.0.1", 12000 )		# will send messages to ip:port
+	oscsndr.setup( "192.168.2.147", 12000 )		# will send messages to ip:port
 	oscsndr.start()							# enabling emission
 
 #func _process(delta):
@@ -21,9 +21,9 @@ func _exit_tree ( ):
 	oscsndr.stop()							# disable the sender, highly recommended!
 
 func _on_XRToolsInteractableAreaButton_button_pressed(button):
-	var fire = get_node( "../campfire")
-	if fire.scale < Vector3(2,2,2):
-		fire.scale *= 1.4
+#	var fire = get_node( "../campfire")
+#	if fire.scale < Vector3(2,2,2):
+#		fire.scale *= 1.4
 	oscsndr.msg("/gdosc")					# creation of new message internally
-	oscsndr.add( self )						# adding an object, will send its name
+	oscsndr.add( "start" )						# adding an object, will send its name
 	oscsndr.send()	
