b53aa47a761edc09adce79acc8a9dcc96f8f339b
Alex MacLean <maclean199@gmail.com>
2023-03-12 09:50:58 -0400

    OSC to Pd works again

--- godot/assets/scripts/osc_sender.gd
+++ godot/assets/scripts/osc_sender.gd
@@ -9,12 +9,18 @@ func _ready():
 	cube = get_node("../cube")
 	cube2 = get_node("../cube2")
 	oscsndr = load("res://addons/gdosc/gdoscsender.gdns").new()
-	oscsndr.setup( "192.168.2.147", 12000 )
+	oscsndr.setup( "127.0.0.1", 12000 )
 	oscsndr.start()
 
 func _process(delta):
 	cube.rotate_y( delta * 1 )
 	cube2.rotate_x( delta * 1 )
+#	oscsndr.msg("/gdosc")					# creation of new message internally
+#	oscsndr.add( cube )						# adding an object, will send its name
+#	oscsndr.add( cube.rotation )			# adding a vector3, will be transform into 3 floats
+#	oscsndr.add( cube.translation )
+#	oscsndr.send()	
+
 
 func _exit_tree ( ):
 	oscsndr.stop()							# disable the sender, highly recommended!
