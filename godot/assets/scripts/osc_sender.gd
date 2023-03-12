8749598fae11eec1a1abc31abca3e792277bab8f
Alex MacLean <maclean199@gmail.com>
2023-03-12 11:36:22 -0400

    Software synths sounding much better

--- godot/assets/scripts/osc_sender.gd
+++ godot/assets/scripts/osc_sender.gd
@@ -3,11 +3,12 @@ extends Node
 var oscsndr
 var cube
 var cube2
+var fire_light
 
 func _ready():
-
 	cube = get_node("../cube")
 	cube2 = get_node("../cube2")
+	fire_light = get_node("../campfire/OmniLight")
 	oscsndr = load("res://addons/gdosc/gdoscsender.gdns").new()
 	oscsndr.setup( "127.0.0.1", 12000 )
 	oscsndr.start()
@@ -15,12 +16,6 @@ func _ready():
 func _process(delta):
 	cube.rotate_y( delta * 1 )
 	cube2.rotate_x( delta * 1 )
-#	oscsndr.msg("/gdosc")					# creation of new message internally
-#	oscsndr.add( cube )						# adding an object, will send its name
-#	oscsndr.add( cube.rotation )			# adding a vector3, will be transform into 3 floats
-#	oscsndr.add( cube.translation )
-#	oscsndr.send()	
-
 
 func _exit_tree ( ):
 	oscsndr.stop()							# disable the sender, highly recommended!
@@ -31,7 +26,13 @@ func _on_XRToolsInteractableAreaButton_button_pressed(button):
 	oscsndr.send()
 
 func _on_InteractableSlider_slider_moved(position):
-	print(position)
+	fire_light.omni_range = position * 40
+	print(fire_light.omni_range)
 	oscsndr.msg("/slider")
 	oscsndr.add(position)
+	oscsndr.send()
+
+func _on_InteractableAreaButton_button_pressed(button):
+	print("BUTTON PRESSED")
+	oscsndr.msg("/fm")
 	oscsndr.send()
